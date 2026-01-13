fca78fdda8c6e6a8d721fd064c19fb844ec7db7d
Dino0204 <s24020@gsm.hs.kr>
2026-01-13 16:14:45 +0900

    fix: root path

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: "C:/Users/skagh/OneDrive/바탕 화면/김준혁/프로젝트/MultiGame/client",
+    root: "./",
   },
 };
 
