037ef2fba3bf4c299ef2a05b3d4392bebbbd5d90
Dino0204 <s24020@gsm.hs.kr>
2026-02-03 21:13:16 +0900

    feat: react-oauth

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -1,3 +1,23 @@
-export default function SignIn(){
-    return <div>Sign In Page</div>;
+"use client";
+
+import { api } from "@/shared/api/server";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const router = useRouter();
+
+  const signin = useGoogleLogin({
+    onSuccess: async (codeResponse) => {
+      console.log(codeResponse);
+      const response = await api.auth.google.post({ code: codeResponse.code });
+      if (response.data?.success) {
+        router.push("/lobby");
+      }
+      return response;
+    },
+    flow: "auth-code",
+  });
+
+  return <button onClick={signin}>Sign in with Google</button>;
 }
