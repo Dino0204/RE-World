21355520c974a156d307332ce666087dcd8cee2a
Dino0204 <s24020@gsm.hs.kr>
2026-02-06 11:44:03 +0900

    feat: signin window

--- client/shared/ui/movableWindow.tsx
+++ client/shared/ui/movableWindow.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import SignIn from "@/features/signin/ui";
 import { useMovable } from "../model/useMovable";
 
-export function MovableWindow() {
-  const { position, isDragging, handleMouseDown } = useMovable();
+interface MovableWindowProps {
+  id?: string;
+  children: React.ReactNode;
+}
+
+export function MovableWindow({
+  id = "default",
+  children,
+}: MovableWindowProps) {
+  const { position, isDragging, handleMouseDown } = useMovable(id);
 
   return (
     <div
@@ -13,23 +20,12 @@ export function MovableWindow() {
         transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
       }}
       className={`
-          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-64 h-40 rounded-lg
-          cursor-grab active:cursor-grabbing z-50
-        bg-brand-beige-mid/40 border-2 border-brand-beige-dark p-1 shadow-sm group backdrop-blur-sm
+          absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-max max-h-max rounded-lg
+          cursor-grab active:cursor-grabbing z-50 overflow-hidden
           ${isDragging ? "will-change-transform select-none" : ""}
         `}
     >
-      <div className="bg-brand-charcoal text-brand-beige px-3 text-xs font-bold tracking-widest mb-1 flex justify-between">
-        <span>Authorize</span>
-      </div>
-      <div className="px-4">
-        <div className="space-y-2">
-          <label className="text-[10px] text-brand-charcoal-light tracking-[0.2em] uppercase font-bold">
-            Operation Mode
-          </label>
-          <SignIn />
-        </div>
-      </div>
+      {children}
     </div>
   );
 }
