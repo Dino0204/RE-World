2109f9927f433e442bac590807f81173f97ade21
Dino0204 <s24020@gsm.hs.kr>
2026-02-13 04:00:21 +0900

    feat: 로비 접근 시 웹 소캣 연결

--- client/app/lobby/page.tsx
+++ client/app/lobby/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { Header } from "@/widgets/lobby/ui/header";
 import { Footer } from "@/widgets/lobby/ui/footer";
 import { Main } from "@/widgets/lobby/ui/main";
 import { Background } from "@/widgets/lobby/ui/background";
+import { useEffect } from "react";
+import { useSocketStore } from "@/shared/model/socket.store";
 
 export default function Lobby() {
+  const connect = useSocketStore((state) => state.connect);
+
+  useEffect(() => {
+    connect();
+  }, [connect]);
+
   return (
     <div className="relative w-full h-screen bg-brand-beige text-brand-charcoal overflow-hidden font-mono select-none flex flex-col">
       <Background />
