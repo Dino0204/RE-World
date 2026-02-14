1ce559d805d64ee2fa04e92cfaed050995c1ee5f
Dino0204 <s24020@gsm.hs.kr>
2026-02-14 14:21:56 +0900

    feat: pistol

--- client/src/entities/weapon/model/weapon.data.ts
+++ client/src/entities/weapon/model/weapon.data.ts
@@ -27,6 +27,43 @@ export const M416: Weapon = {
       { x: -0.008, y: 0.025 },
     ],
   },
+  transform: {
+    rotation: [0, Math.PI, 0],
+    scale: 100,
+  },
 };
 
-export const WEAPONS = [M416];
+export const PISTOL: Weapon = {
+  name: "PISTOL",
+  type: "권총",
+  model: "Pistol",
+  damage: 20,
+  fireRate: 100,
+  ammo: {
+    type: "9mm",
+    amount: 30,
+    maxAmount: 30,
+  },
+  parts: [],
+  recoil: {
+    vertical: 0.02,
+    horizontal: 0.01,
+    maxVertical: 0.3,
+    maxHorizontal: 0.5,
+    pattern: [
+      { x: 0, y: 0.01 },
+      { x: 0.002, y: 0.012 },
+      { x: -0.002, y: 0.015 },
+      { x: 0.005, y: 0.018 },
+      { x: -0.005, y: 0.02 },
+      { x: 0.008, y: 0.022 },
+      { x: -0.008, y: 0.025 },
+    ],
+  },
+  transform: {
+    rotation: [Math.PI / 2, Math.PI, 0],
+    scale: 100,
+  },
+};
+
+export const WEAPONS = [M416, PISTOL];
