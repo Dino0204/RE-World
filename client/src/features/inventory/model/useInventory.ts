a875e2502e2664676e4c37573fb29d4090034400
Dino0204 <s24020@gsm.hs.kr>
2026-02-17 12:51:41 +0900

    feat: ESC를 눌렀을 때 인벤토리 닫기

--- client/src/features/inventory/model/useInventory.ts
+++ client/src/features/inventory/model/useInventory.ts
@@ -2,17 +2,21 @@ import { useEffect } from "react";
 import { useInventoryStore } from "./inventory.store";
 
 export const useInventory = () => {
-  const { toggleOpen } = useInventoryStore();
+  const { toggleOpen, close } = useInventoryStore();
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key == "i" || event.key == "I") {
+      const key = event.key;
+      if (key == "i" || key == "I") {
         event.preventDefault();
         toggleOpen();
+      } else if (key == "escape" || key == "Escape") {
+        event.preventDefault();
+        close();
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [toggleOpen]);
+  }, [close, toggleOpen]);
 };
