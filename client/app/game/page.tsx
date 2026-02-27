d603123c81490a24a71ddbd30899fbe90b1df6f6
Dino0204 <s24020@gsm.hs.kr>
2026-02-27 16:06:35 +0900

    feat: 항구 맵

--- client/app/game/page.tsx
+++ client/app/game/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import FlatGround from "@/entities/world/ui/flat";
-import Target from "@/entities/target/ui/target";
 import Player from "@/entities/player/ui/player";
 import { Sky, View, KeyboardControls } from "@react-three/drei";
 import type { KeyboardControlsEntry } from "@react-three/drei";
@@ -19,6 +17,7 @@ import { useSocketStore } from "@/shared/model/socket.store";
 import { InventoryHUD } from "@/features/inventory/ui/inventory-hud";
 import { Minimap } from "@/widgets/minimap/ui/minimap";
 import { Controls } from "@/entities/player/model/player.constants";
+import PortGround from "@/entities/world/ui/port";
 
 export default function GamePage() {
   const container = useRef<HTMLDivElement>(null!);
@@ -45,40 +44,33 @@ export default function GamePage() {
 
   return (
     <KeyboardControls map={map}>
-    <div className="w-screen h-screen" ref={container}>
-      <LoadingScreen />
-      <GameHUD />
-      <InventoryHUD />
+      <div className="w-screen h-screen" ref={container}>
+        <LoadingScreen />
+        <GameHUD />
+        <InventoryHUD />
 
-      {/* game view */}
-      <View className="absolute inset-0">
-        <Sky sunPosition={[100, 20, 100]} />
-        <ambientLight intensity={1.5} />
-        <Physics debug gravity={[0, -20, 0]}>
-          <BulletManager />
-          <ImpactManager />
-          <MultiplayerManager />
-          <Target id="target-1" position={[0, -4.5, -10]} />
-          <Target id="target-2" position={[5, -4.5, -15]} />
-          <Target id="target-3" position={[-5, -4.5, -5]} />
-          <Player />
-          <FlatGround />
-        </Physics>
-      </View>
+        {/* game view */}
+        <View className="absolute inset-0">
+          <Sky sunPosition={[100, 20, 100]} />
+          <ambientLight intensity={1.5} />
+          <Physics gravity={[0, -20, 0]}>
+            <BulletManager />
+            <ImpactManager />
+            <MultiplayerManager />
+            <Player />
+            <PortGround />
+          </Physics>
+        </View>
 
-      {/* mini-map */}
-      <View className="absolute w-100 h-100 top-5 left-5 z-10 shadow-2xl">
-        <Minimap />
-      </View>
+        {/* mini-map */}
+        <View className="absolute w-100 h-100 top-5 left-5 z-10 shadow-2xl">
+          <Minimap />
+        </View>
 
-      <Canvas
-        // style={{ position: "fixed", inset: 0, pointerEvents: "none" }}
-        gl={{ alpha: true }}
-        eventSource={container}
-      >
-        <View.Port />
-      </Canvas>
-    </div>
+        <Canvas gl={{ alpha: true }} eventSource={container}>
+          <View.Port />
+        </Canvas>
+      </div>
     </KeyboardControls>
   );
 }
