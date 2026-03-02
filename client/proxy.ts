3bf7d2df9e7b2936def2029cfb6277b464f56213
Dino0204 <s24020@gsm.hs.kr>
2026-03-03 07:55:47 +0900

    chore: middleware to proxy

--- client/proxy.ts
+++ client/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server";
 
 const PUBLIC_ROUTES = ["/", "/lobby"];
 
-export const middleware = (request: NextRequest) => {
+export const proxy = (request: NextRequest) => {
   const { pathname } = new URL(request.url);
 
   // 퍼블릭 라우트가 아닌 경우 인증 토큰 확인
@@ -20,14 +20,5 @@ export const middleware = (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: [
-    /*
-     * 다음으로 시작하는 경로를 제외한 모든 요청 경로를 매칭합니다:
-     * - api (API 라우트)
-     * - _next/static (정적 파일)
-     * - _next/image (이미지 최적화 파일)
-     * - favicon.ico (파비콘 파일)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|models).*)",
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|models).*)"],
 };
