bdd0f1dec26f347ff04c6e0c66b127aabc82c418
Dino0204 <s24020@gsm.hs.kr>
2026-03-04 14:44:38 +0900

    feat: agentation tool bar

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Agentation } from "agentation";
 
 export const metadata: Metadata = {
   title: "RE-World",
@@ -15,6 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {process.env.NODE_ENV === "development" && (
+          <Agentation endpoint="http://localhost:4747" />
+        )}
         <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_ID!}>
           {children}
         </GoogleOAuthProvider>
