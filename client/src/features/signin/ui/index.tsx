f56fcd8f310cefb7f5c0900235b0cb933874e565
Dino0204 <s24020@gsm.hs.kr>
2026-03-13 00:21:51 +0900

    fix: 유저 로그인 후 정보가 바로 나타나지 않는 문제

--- client/src/features/signin/ui/index.tsx
+++ client/src/features/signin/ui/index.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { api } from "@/shared/api/server";
+import { useUserStore } from "@/entities/user/model/user.store";
 import { Button } from "@/shared/ui/button";
 import { useGoogleLogin } from "@react-oauth/google";
 import { useRouter } from "next/navigation";
 
 export default function SignIn() {
   const router = useRouter();
+  const { setUser } = useUserStore();
 
   const signin = useGoogleLogin({
     onSuccess: async (response) => {
       const { data } = await api.auth.google.post({ code: response.code });
 
-      if (data?.success) {
+      if (data?.success && data.user) {
+        setUser(data.user);
         router.push("/lobby");
       }
 
